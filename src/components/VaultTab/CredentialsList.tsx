4e4c645fa82f88800024e41e33c92e6816f7f3e9
Kevin Mallari <kevinmallari62@gmail.com>
2023-01-03 23:00:38 +0800

    continue vault tab

--- src/components/VaultTab/CredentialsList.tsx
+++ src/components/VaultTab/CredentialsList.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Credentials } from "../../types";
 import { BiCopy } from "react-icons/bi";
+import axios from "axios";
+import _ from "lodash";
 interface CredentialsListProps {
   credentialsList: Credentials[] | undefined;
   isSuggested?: boolean;
@@ -12,6 +14,28 @@ export const CredentialsList: React.FC<CredentialsListProps> = ({
   isSuggested = false,
   size,
 }) => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [copyText, setCopyText] = useState("Copy info");
+
+  const updateUsedAt = (id: string) => {
+    axios
+      .put(apiUrl + `/id/${id}/`, {
+        updated_at: Date.now(),
+      })
+      .then((res) => {
+        console.log({ res });
+      });
+    setCopyText("Info copied!");
+    setTimeout(() => {
+      setCopyText("Copy info");
+    }, 1000);
+  };
+
+  const debouncedUpdateUsedAt = useMemo(
+    () => _.debounce(updateUsedAt, 150),
+    []
+  );
+
   return (
     <div className='self-start relative'>
       <h2 className='font-bold text-sm sticky self-start border-b border-slate-200 px-4 pb-2'>
@@ -20,7 +44,7 @@ export const CredentialsList: React.FC<CredentialsListProps> = ({
         ) : (
           <div className='flex flex-row justify-between'>
             <span>All logins</span>
-            <span className="font-medium">{size} items</span>
+            <span className='font-medium'>{size} items</span>
           </div>
         )}
       </h2>
@@ -46,13 +70,31 @@ export const CredentialsList: React.FC<CredentialsListProps> = ({
             </div>
             <div className='group/copy relative'>
               <div className='invisible group-hover/copy:visible absolute  top-0 bottom-0 my-auto'>
-                <div className='absolute w-3 h-3 bg-blue-700 right-0 rotate-45 top-0 bottom-0 my-auto -left-5'></div>
-                <div className='bg-blue-700 text-white font-medium text-xs  h-fit px-2 py-1 rounded-md top-1 absolute -left-20'>
-                  Copy info
+                <div
+                  className={`absolute w-3 h-3  right-0 rotate-45 top-0 bottom-0 my-auto -left-5 ${
+                    copyText === "Copy info" ? "bg-blue-700" : "bg-blue-500"
+                  }`}
+                ></div>
+                <div
+                  className={`text-white font-medium text-xs h-fit px-2 py-1 rounded-md top-1 absolute ${
+                    copyText === "Copy info"
+                      ? "w-[4.5rem] -left-[5.2rem] bg-blue-700"
+                      : "w-[5.5rem] -left-[6.25rem] bg-blue-500"
+                  }`}
+                >
+                  {copyText}
                 </div>
               </div>
 
-              <button className='p-2 rounded-lg hover:bg-blue-300 hidden group-hover/container:block'>
+              <button
+                className='p-2 rounded-lg hover:bg-blue-300 hidden group-hover/container:block'
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `${creds.login}:${creds.password}`
+                  );
+                  debouncedUpdateUsedAt(creds.id);
+                }}
+              >
                 <BiCopy />
               </button>
             </div>
